d6611fcdc9c13470aad1775340ca9dacd784508f
fred13kim <fred13kim@gmail.com>
2024-03-06 23:02:15 -0500

    add q1 sln

--- InClassExercise/q1.asm
+++ InClassExercise/q1.asm
@@ -3,6 +3,7 @@
     A: .word 9, 8, 7, 6, 5
     B: .word 0, 1, 2, 3, 4
     g: .word 5
+    newline: .asciiz "\n"
 
 .text
 .globl main
@@ -13,10 +14,19 @@ main:
     la $s7, B
     
 ### Your code for Q1 goes here ###
+    lw $t0, 16($s7)
+    sll $t0, $t0, 2
+    addu $t0, $t0, $s6 #*note: address arithmetic should be done unsigned
+    lw $t0, 0($t0)
+    sub $s0, $s1, $t0
 
 ### Print S0 Register ###
     move $a0, $s0
     li $v0, 1
+    syscall
+
+    la $a0, newline
+    li $v0, 4
     syscall
 
 ### Exit Program ###
